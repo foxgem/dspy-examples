821154684c4aabe1d5c6828053213186355291f0
foxgem <jianhgreat@hotmail.com>
2024-08-24 22:15:00 +1000

    optimize the summary example with signature class.

--- html_summary/summary.py
+++ html_summary/summary.py
@@ -10,15 +10,26 @@ load_dotenv()
 gpt4o = dspy.OpenAI(model="gpt-4o-2024-08-06", max_tokens=1024)
 dspy.configure(lm=gpt4o)
 
-summary = dspy.ChainOfThoughtWithHint("document -> summary")
+
+class Summary(dspy.Signature):
+    """Summarize a document:
+    1. The summary should be written in the same language used by the document.
+    2. The summary should list 3~5 key points from the document.
+    3. The summary should not miss the information that the author wants to highlight.
+    """
+
+    document = dspy.InputField(desc="The document to summarize.")
+    summary = dspy.OutputField(desc="The summary of the document.")
+
+
+summary = dspy.ChainOfThought(Summary)
 document = extract_article_content(
-    "https://blog.dteam.top/posts/2024-02/go-ethereum-cookbook.html"
+    "https://towardsdatascience.com/automating-prompt-engineering-with-dspy-and-haystack-926a637a3f43"
 )
 
 
 response = summary(
     document=document.content,
-    hint="the summary should be written in the same language used by document",
 )
 
 print(document.title, ":")
